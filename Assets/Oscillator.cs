2807902c9dd54307492c615b38c2158b991423c8
Nik Hammer-Ellis <nikhammerellis@gmail.com>
2018-01-31 09:44:06 -0800

    game finished, 3 levels only so far

---
+++ Assets/Oscillator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour {
+
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float period = 2f;
+
+    // todo remove from inspector later 
+    //[Range(0,1)][SerializeField] below doesn't need to be exposed to the inspector 
+    float movementFactor; // 0 for not moved, 1 for fully moved 
+
+    Vector3 startingPos;
+
+	// Use this for initialization
+	void Start () 
+    {
+        startingPos = transform.position; //get the transform (xyz start position) of the obstacle the script is attached to 
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        // set movement factor 
+        if (period <= Mathf.Epsilon) { return; } //protect against period == 0 (always use Mathf.Epsilon to compare floating point to 0)
+        float cycles = Time.time / period; //this grows continually from 0
+
+        const float tau = Mathf.PI * 2; //about 6.28 (double Pi obviously)
+        float rawSinWave = Mathf.Sin(cycles * tau); //goes from -1 to 1 
+
+        movementFactor = rawSinWave / 2f + 0.5f;
+        Vector3 offset = movementFactor * movementVector;
+        transform.position = startingPos + offset;
+	}
+}
--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,19 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour {
+ 
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
+    [SerializeField] float levelLoadDelay = 2f;
+
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip success;
+    [SerializeField] AudioClip death;
+
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem successParticles;
+    [SerializeField] ParticleSystem deathParticles;
 
     Rigidbody rigidBody;
     AudioSource audioSource;
 
-    //Play the music
-    bool play;
-    //Detect when you use the toggle, ensures music isn’t played multiple times
-    bool toggleChange;
+    //enum State { Alive, Dying, Transcending } //apparently this was over-engineered 
+    //State state = State.Alive; //new implementation below 
+
+    bool isTransitioning = false;
+    bool collisionsEnabled = true;
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+    {
         rigidBody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
 	}
@@ -21,31 +34,126 @@ public class Rocket : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        ProcessInput();
+        if (!isTransitioning) //previous implementation: (state == State.Alive)
+        {
+            RespondToThrustInput();
+            RespondToRotateInput();
+        }
+        if (Debug.isDebugBuild) //knows if the build is a Debug Build or not, prevents a deployed game from allowing debug keys 
+        {
+            RespondToDebugKeys();
+        }
     }
 
-    private void ProcessInput()
+    private void RespondToDebugKeys()
     {
-        if(Input.GetKey(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.L))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
-            if(!audioSource.isPlaying) //prevents layering 
-            {
-                audioSource.Play();
-            }
+            LoadNextLevel();
         }
-        else 
+        else if (Input.GetKeyDown(KeyCode.C))
         {
-            audioSource.Stop();
+            collisionsEnabled = !collisionsEnabled; //toggle collision 
         }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (isTransitioning || !collisionsEnabled) { return; } //ignore collisions when dead, previous implementation: (state != State.Alive ||
+
+        switch(collision.gameObject.tag)
+        {
+            case "Friendly":
+                //do nothing
+                break;
+            case "Finish":
+                StartSuccessSequence();
+                break;
+            default:
+                StartDeathSequence();
+                break;
+        }
+    }
+
+    private void StartSuccessSequence()
+    {
+        isTransitioning = true; //state = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(success);
+        successParticles.Play();
+        Invoke("LoadNextLevel", levelLoadDelay); 
+    }
+
+    private void StartDeathSequence()
+    {
+        isTransitioning = true; //state = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(death);
+        deathParticles.Play();
+        Invoke("LoadFirstLevel", levelLoadDelay);
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex; //find the build index of the current level 
+        int nextSceneIndex = currentSceneIndex + 1; //add 1 to build index, so that you can progress to the next level 
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings) {
+            nextSceneIndex = 0; // or just trigger LoadFirstLevel and put the below in an else statement, but this does seem cleaner
+        } 
+        SceneManager.LoadScene(nextSceneIndex); //load the next level
+
+         
+    }
+
+    private void RespondToThrustInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            ApplyThrust();
+        }
+        else
+        {
+            StopApplyingThrust();
+        }
+    }
+
+    private void StopApplyingThrust()
+    {
+        audioSource.Stop();
+        mainEngineParticles.Stop();
+    }
+
+    private void ApplyThrust()
+    {
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+        if (!audioSource.isPlaying) //prevents layering 
+        {
+            audioSource.PlayOneShot(mainEngine);
+        }
+        mainEngineParticles.Play();
+    }
+
+    private void RespondToRotateInput()
+    {
+        //rigidBody.freezeRotation = true; //take manual control of rotation (taking advantage of a side effect, not ideal)
+        rigidBody.angularVelocity = Vector3.zero; //remove rotation due to physics 
+
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
+
+        rigidBody.freezeRotation = false; //resume physics control (taking advantage of a side effect, not ideal)
     }
 }
